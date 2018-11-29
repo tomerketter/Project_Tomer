e982cf9cff074dff3e0208e8abea8cf6b80c9f1d
tomerketter <tketter@ccom.unh.edu>
2018-11-29 14:01:37 -0500

    First setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='project_tomer',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/tomerketter/project_tomer',
+      author='Tomer Ketter',
+      author_email='tketter@ccom.unh.edu',
+      license='MIT',
+      packages=['ext_depth'],
+      zip_safe=False)
