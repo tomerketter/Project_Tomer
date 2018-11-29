4ddd445b51a8d7b45f4865be2ac153326b6c2a85
tomerketter <tketter@ccom.unh.edu>
2018-11-29 13:53:33 -0500

    my first python files

---
+++ ext_depth/common.py
@@ -0,0 +1,13 @@
+def print_name():
+    name="Tomer"
+    print(name)
+
+def second_func():
+    print("Kevin is in the room")
+
+class Human:
+    def __init__(self):
+        self.name="Unknown"
+    def print_name(self):
+        print(self.name)
+
